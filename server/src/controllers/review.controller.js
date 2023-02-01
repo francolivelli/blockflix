887981085d1f1a8bebee4ede1791b18814345aac
francolivelli <franco.livelli@gmail.com>
2023-02-01 16:41:04 -0300

    person & review controllers

---
+++ server/src/controllers/review.controller.js
@@ -0,0 +1,58 @@
+import responseHandler from "../handlers/response.handler";
+import reviewModel from "../models/review.model";
+
+const create = async (req, res) => {
+  try {
+    const { moviedId } = req.params;
+
+    const review = new reviewModel({
+      user: req.user.id,
+      moviedId,
+      ...req.body,
+    });
+
+    await review.save();
+
+    responseHandler.created(res, {
+      ...review._doc,
+      id: review.id,
+      user: req.user,
+    });
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    const review = await reviewModel.findOne({
+      _id: review.id,
+      user: req.user.id,
+    });
+
+    if (!review) return responseHandler.notFound(res);
+
+    await review.remove();
+
+    responseHandler.ok(res);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const getReviewsOfUser = async (req, res) => {
+  try {
+    const reviews = await reviewModel
+      .find({
+        user: req.user.id,
+      })
+      .sort("-createdAt");
+
+    responseHandler.ok(res, reviews);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+export default { create, remove, getReviewsOfUser };
